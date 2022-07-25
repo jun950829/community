c524ead215bb2a9e52e3c68a1b36c15dee9f8447
Cheon SeungJun <sjc0829@naver.com>
2022-07-25 13:26:51 +0900

    [MOD]vue3쪽으로 세팅

---
+++ backend/dist/precache-manifest.5275c6ac09208094c6d7ff4247aef86e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5fe6c15b887cf7715b",
+    "url": "/css/app.3ec98f28.css"
+  },
+  {
+    "revision": "dfe73ddf73e146bfe158ba0fcce8776d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f31bee24a3ae87b423",
+    "url": "/js/about.9e58025a.js"
+  },
+  {
+    "revision": "6b5fe6c15b887cf7715b",
+    "url": "/js/app.23b87749.js"
+  },
+  {
+    "revision": "b260616e3c24eb87dd0d",
+    "url": "/js/chunk-vendors.1b414513.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
