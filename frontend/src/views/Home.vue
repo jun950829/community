507fb466f54431970efca342ca831855c9b5660c
Cheon SeungJun <sjc0829@naver.com>
2022-07-29 17:02:28 +0900

    [MOD] 수정작업

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,44 +1,57 @@
 <template>
   <div class="home">
     <img alt="Vue logo" :src="logo" />
-    <div>{{isLogin}}</div>
+    <div>{{ isLogin }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-  interface httpRespond {
-    status: String,
-    user_data : userData
+import { ref } from 'vue'
+
+let isLogin = ref(false);
+
+interface httpRespond {
+  status: string;
+  user_data: userDataForm;
+}
+
+interface userDataForm {
+  user_id: string;
+  user_password: string;
+  user_name: string;
+  user_nickname: string;
+}
+
+async function checkLogin(): Promise<httpRespond> {
+  const response = await window.fetch("/api/user/data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user_id: "test4",
+    }),
+  });
+
+  const result = await response.json();
+  return result;
+}
+
+(async () => {
+  try {
+    let userData = await checkLogin();
+
+    if (userData.user_data.user_id == "test4") {
+      console.log("true");
+      isLogin = true;
+      return;
+    }
+  } catch (e) {
+    console.log(e);
+
+    return;
   }
-
-  interface userData {
-    user_id: String,
-    user_password: String,
-    user_name: String,
-    user_nickname : String
-  }
-
-  async function checkLogin(this: any): Promise<httpRespond> {
-    const response = await window.fetch("/api/user/data", {
-      method: 'POST',
-      headers: {
-        'Content-Type' : 'application/json',
-      },
-      body: JSON.stringify({
-        user_id : 'test4',
-      }),
-    })
-
-    const result = await response.json();
-    return result;
-  }
-
-  let userData = await checkLogin();
-
-  if (userData.user_data.user_id == 'test4') {
-    console.log('true');
-  }
-
+})();
 </script>
 
 <script lang="ts">
@@ -48,8 +61,6 @@ export default defineComponent({
   name: "Home",
   data: () => ({
     logo: require("../assets/logo.png"),
-    isLogin : false,
   }),
-
 });
 </script>
